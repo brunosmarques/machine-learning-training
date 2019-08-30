247e18f8efaf69e47aa54b3b08b5654a81c7fb2e
Bruno Schmidt Marques <bruno@brunomarques.com.br>
2019-08-30 08:33:14 -0300

    Refactoring code

--- generic-image-classifier/app.py
+++ generic-image-classifier/app.py
@@ -1,47 +1,56 @@
+# General tools imports
 from __future__ import division, print_function
 import os
 import glob
 import re
 import numpy as np
 
-from flask import Flask
+# Flask import
 from flask import Flask, redirect, url_for, request, render_template
 from werkzeug.utils import secure_filename
 
+# Keras models and tools import
 from keras.models import load_model
 from keras.applications.resnet50 import ResNet50
 from keras.preprocessing import image
 from keras.applications.imagenet_utils import preprocess_input, decode_predictions
 
+# Tensorflow import
 import tensorflow 
+
+# Tensorflow workaround (needed for Win10)
 global graph,model
 graph = tensorflow.get_default_graph()
 
-
-
+# Flash configuration
 app = Flask(__name__)
+
+# Model ResNet50 (generic purpose image model already trained)
 MODEL_PATH = 'models/your_model.h5'
 model = ResNet50(weights='imagenet')
 
 def model_predict(img_path, model):
-        #img_path = "C:\\Users\\Bruno\\Desktop\\car.jpg"
-        print(img_path)
-        img = image.load_img(img_path, target_size=(224, 224))
-        
-        x = image.img_to_array(img)
-        #x = np.true_divide(x, 255)
-        x = np.expand_dims(x, axis=0)
-        x = preprocess_input(x, mode='caffe')
+    """Predict category of an image based on a image path and a model"""
 
-        with graph.as_default():
-            preds = model.predict(x)        
+    # image normalization/manipulation
+    img = image.load_img(img_path, target_size=(224, 224))    
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x, mode='caffe')
 
-        return preds
+    # do the prediction
+    with graph.as_default():
+        preds = model.predict(x)        
 
+    # returns the prediction
+    return preds
+
+# Default Flask route, index.html
 @app.route('/')
 def index():
     return render_template('index.html')
 
+# Predict Flask route, actioned by button in index
 @app.route('/predict', methods=['GET', 'POST'])
 def upload():
     if request.method == 'POST':
